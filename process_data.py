80f4faa20f023d4d3f24922c25f0692d94b450a3
Ben Palmer <b.c.palmer@northumbria.ac.uk>
2024-07-11 13:04:10 +0100

    organised images + chart additions

--- process_data.py
+++ process_data.py
@@ -5,6 +5,7 @@ import re
 from datetime import datetime
 from glob import glob
 
+import cartopy
 import cartopy.crs as ccrs
 import geopandas as gpd
 import matplotlib.pyplot as plt
@@ -12,27 +13,28 @@ import numpy as np
 import pandas as pd
 from netCDF4 import Dataset  # pylint:disable=no-name-in-module
 
-# from scipy.ndimage import uniform_filter1d
-from shapely.geometry import Point
-
 file_years_re = re.compile("[0-9]{6}")
 
-csv_columns = ["code", "period", "midpoint", "basin", "Raw SEC"]
+month_key = {
+    "10": {"month": 0, "winter_year": 0},
+    "11": {"month": 1, "winter_year": 0},
+    "12": {"month": 2, "winter_year": 0},
+    "01": {"month": 3, "winter_year": 1},
+    "02": {"month": 4, "winter_year": 1},
+    "03": {"month": 5, "winter_year": 1},
+    "04": {"month": 6, "winter_year": 1},
+}
 
 
-def plot_arco_thickness(
-    x,
-    y,
-    values,
-):
+def plot_arco_thickness(x, y, values):
     crs_new = ccrs.NorthPolarStereo(central_longitude=0)
 
     fig = plt.figure(figsize=(9, 7))
-    ax = plt.axes(facecolor="white", projection=crs_new)
+    ax = plt.axes(facecolor="whitesmoke", projection=crs_new)
 
     ax.coastlines(resolution="110m", linewidth=0.5)
 
-    gl = ax.gridlines(draw_labels=True, color="black", alpha=0.4, linestyle="dashed")
+    gl = ax.gridlines(draw_labels=True, color="black", alpha=0.4, linestyle="dashed", linewidth=0.5)
     ax.set_extent([-180, 180, 50, 90], crs=ccrs.PlateCarree())
     gl.ylabel_style = {
         "color": "black",
@@ -50,6 +52,7 @@ def plot_arco_thickness(
         ),
         shading="gouraud",
     )
+    ax.add_feature(cartopy.feature.LAND, color="gainsboro", zorder=1)
     fig.colorbar(cs, ax=ax)
 
     return fig
@@ -58,11 +61,6 @@ def plot_arco_thickness(
 def get_data_files(folder: str):
     for f in sorted(glob(os.path.join(folder, "*.nc"))):
         yield f
-
-
-def get_mean_data(gp_dataframe: gpd.GeoDataFrame) -> float:
-    mean_thickness = gp_dataframe[gp_dataframe.notna()]["Thickness"].mean()
-    return mean_thickness
 
 
 def time_to_ts(time):
@@ -82,94 +80,74 @@ if __name__ == "__main__":
         "-p", "--processed_file_dir", help="Directory for processed files made here"
     )
     parser.add_argument(
-        "-t", "--time_series_file", help="Path to the csv file containing the time series data"
+        "-x", "--aux_file_dir", help="Directory where auxiliary files are kept (shapefiles, ...)"
     )
 
     argv = parser.parse_args()
 
-    image_dir = argv.processed_file_dir
+    image_dir = os.path.join(argv.processed_file_dir, "images")
     data_file_dir = argv.data_file_dir
-    time_series_file = argv.time_series_file
+    time_series_file = os.path.join(argv.processed_file_dir, "time_series_data.csv")
+    aux_file_dir = argv.aux_file_dir
 
     image_files = glob("*.png", root_dir=image_dir)
 
     seaice_files = get_data_files(data_file_dir)
 
-    crs = "epsg:3413"
-
     all_data = []
 
-    try:
-        for file_name in seaice_files:
-            print("Processing ", file_name, end="", flush=True)
+    arctic_regions = gpd.read_file(
+        os.path.join(aux_file_dir, "our_arctic_regions", "arctic_regions.shp")
+    )
 
-            years = file_years_re.findall(file_name)
-            if len(years) == 0:
-                continue
-            file_year: str = years[0]
-            # 199107-199607
-            fmt_year = f"{file_year[0:4]}/{file_year[4:6]}"
+    for file_name in seaice_files:
+        print("Processing ", file_name, flush=True)
 
-            # load nc
-            nc = Dataset(file_name)
-            x_values = nc["xc"][:].data
-            y_values = nc["yc"][:].data
-            thickness = nc["sea_ice_thickness"][:].data[0, :, :]
-            time_bounds = nc["time_bnds"][0, :]
+        file_dates_search = file_years_re.findall(file_name)
+        if len(file_dates_search) == 0:
+            continue
+        file_date: str = file_dates_search[0]
+        # 199107-199607
+        file_year = file_date[0:4]
+        file_month = file_date[4:6]
+        fmt_year = f"{file_year}/{file_month}"
 
-            x_coords, y_coords = np.meshgrid(x_values, y_values, indexing="xy")
-            coords_arr = [Point(x, y) for x, y in zip(x_coords.flatten(), y_coords.flatten())]
+        # load nc
+        nc = Dataset(file_name)
+        x_values = nc["xc"][:].data
+        y_values = nc["yc"][:].data
+        thickness = nc["sea_ice_thickness"][:].data[0, :, :]
+        time_bounds = nc["time_bnds"][0, :]
 
-            # make dataframe here
-            my_data = gpd.GeoDataFrame(
-                data={
-                    "Thickness": thickness.flatten(),
-                    "geometry": coords_arr,
-                },
-                crs=crs,
-            )
+        mean_thickness = np.nanmean(thickness)
 
-            ts_data = get_mean_data(my_data)
-            midpoint = datetime.fromtimestamp(time_bounds[0] + time_bounds[1] / 2)
+        midpoint = datetime.fromtimestamp((time_bounds[0] + time_bounds[1]) / 2)
 
-            all_data.append(
-                {"code": file_year, "period": fmt_year, "midpoint": midpoint, "thickness": ts_data}
-            )
+        all_data.append(
+            {
+                "code": file_date,
+                "year": file_year,
+                "season_year": int(file_year) - month_key[str(file_month)]["winter_year"],
+                "month": midpoint.strftime("%B"),
+                "season_month": month_key[str(file_month)]["month"],
+                "region": "Arctic",
+                "period": fmt_year,
+                "thickness": mean_thickness,
+            }
+        )
 
-            if file_year + ".png" not in image_files:
-                # generate image and save in images folder
-                fig = plot_arco_thickness(x_values, y_values, thickness)
+        if file_date + ".png" not in image_files:
+            # generate image and save in images folder
+            fig = plot_arco_thickness(x_values, y_values, thickness)
 
-                fig_file_path = os.path.join(image_dir, file_year + ".png")
-                print(f"\nSaving figure to {fig_file_path}", end="")
-                fig.savefig(fig_file_path)
-                plt.close()
+            fig_file_path = os.path.join(image_dir, file_date + ".png")
+            print(f"Saving figure to {fig_file_path}")
+            fig.savefig(fig_file_path)
+            plt.close()
 
-            print("")
+    # print all data to csv file
+    # keyboard interrupt still writes all collected data
+    df = pd.DataFrame(all_data)
 
-    except KeyboardInterrupt as e:
-        print("\nRecieved KeyboardInterrupt" + str(e))
-    finally:
-        # print all data to csv file
-        # keyboard interrupt still writes all collected data
-        df = pd.DataFrame(all_data)
-        # df["Smooth SEC"] = np.zeros(len(df))
-        # df["dH"] = np.zeros(len(df))
-
-        # for basin_no in df["basin"].unique():
-        #     basin_sec = df[df["basin"] == basin_no]["Raw SEC"]
-        #     smooth_basins = uniform_filter1d(basin_sec, size=5)
-        #     df.loc[df["basin"] == basin_no, "Smooth SEC"] = smooth_basins
-
-        #     dh_values = np.zeros(len(basin_sec))
-
-        #     for i in range(len(basin_sec)):
-        #         dh_values[i] = np.trapz(basin_sec[: i + 1])
-
-        #     df.loc[df["basin"] == basin_no, "dH"] = dh_values
-
-        # df["Raw SEC"] = df["Raw SEC"].round(3)
-        # df["Smooth SEC"] = df["Smooth SEC"].round(3)
-
-        # save dataframe
-        df.to_csv(time_series_file, sep=",")
+    # save dataframe
+    df.to_csv(time_series_file, sep=",", index=False)
